0bb403d5fe1bde9b3c9aad3acd5d081a88d78413
Mj-br <98741174+Mj-br@users.noreply.github.com>
2023-08-12 20:19:17 +0200

    Adding Hilt and injecting our ViewModel

---
+++ app/src/main/java/com/cursokotlin/appinstaclone/AppInstaCloneApplication.kt
@@ -0,0 +1,8 @@
+package com.cursokotlin.appinstaclone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppInstaCloneApplication : Application() {
+}
--- app/src/main/java/com/cursokotlin/appinstaclone/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/appinstaclone/MainActivity.kt
@@ -10,8 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.cursokotlin.appinstaclone.ui.theme.AppInstaCloneTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +23,7 @@ class MainActivity : ComponentActivity() {
             AppInstaCloneTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    InstagramApp()
                 }
             }
         }
@@ -27,17 +31,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun InstagramApp(){
+    val vm = hiltViewModel<IgViewModel>()
+    val i = 0
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AppInstaCloneTheme {
-        Greeting("Android")
+        InstagramApp()
+
     }
 }
