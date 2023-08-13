51365d2119ac642c4dbeb61e2bdf8e50740b7fc9
Mj-br <98741174+Mj-br@users.noreply.github.com>
2023-08-13 21:07:44 +0200

    Update SignUp Screen
    Adding logo.

--- app/src/main/java/com/cursokotlin/appinstaclone/auth/SignUpScreen.kt
+++ app/src/main/java/com/cursokotlin/appinstaclone/auth/SignUpScreen.kt
@@ -1,18 +1,71 @@
 package com.cursokotlin.appinstaclone.auth
 
+import android.app.Activity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.cursokotlin.appinstaclone.IgViewModel
+import com.cursokotlin.appinstaclone.R
 
 @Composable
-fun SingUpScreen(navController: NavController, vm: IgViewModel){
-    Box(modifier = Modifier.fillMaxWidth()){
-        Text(text = "SignUp Screen")
+fun SingUpScreen(navController: NavController, vm: IgViewModel) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        Header(Modifier.align(Alignment.TopEnd))
+        Body(Modifier.align(Alignment.Center), vm)
     }
-    
 
+
+}
+
+@Composable
+fun Body(modifier: Modifier, vm: IgViewModel) {
+    //Variables to save states with LiveData or Flow
+
+    //Design of the body login screen
+    Column(modifier = modifier) {
+        ImageLogo(Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.size(16.dp))
+
+    }
+
+}
+
+@Composable
+fun ImageLogo(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.insta),
+        contentDescription = "logo",
+        modifier = modifier
+    )
+
+}
+
+@Composable
+fun Header(modifier: Modifier) {
+    val activity = LocalContext.current as Activity
+    Icon(
+        imageVector = Icons.Default.Close,
+        contentDescription = "close app",
+        modifier = modifier.clickable { activity.finish() })
 }
