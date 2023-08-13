ecc808bb9e270de0ba62a3da30e70152498d2b07
Mj-br <98741174+Mj-br@users.noreply.github.com>
2023-08-13 20:51:29 +0200

    Update SignUpScreen.kt

--- app/src/main/java/com/cursokotlin/appinstaclone/auth/SignUpScreen.kt
+++ app/src/main/java/com/cursokotlin/appinstaclone/auth/SignUpScreen.kt
@@ -1,12 +1,18 @@
 package com.cursokotlin.appinstaclone.auth
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.cursokotlin.appinstaclone.IgViewModel
 
 @Composable
 fun SingUpScreen(navController: NavController, vm: IgViewModel){
-    Text(text = "SignUpScreen")
+    Box(modifier = Modifier.fillMaxWidth()){
+        Text(text = "SignUp Screen")
+    }
+    
 
 }
