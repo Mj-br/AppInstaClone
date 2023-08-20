078d7b4a3d131d10fd0c2e793c16ef2e1c9382a1
Mj-br <98741174+Mj-br@users.noreply.github.com>
2023-08-20 19:49:49 +0200

    Added some components to fill MyPostsScreen.kt

--- app/src/main/java/com/cursokotlin/appinstaclone/main/composables/screens/MyPostsScreen.kt
+++ app/src/main/java/com/cursokotlin/appinstaclone/main/composables/screens/MyPostsScreen.kt
@@ -1,10 +1,21 @@
 package com.cursokotlin.appinstaclone.main.composables.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.cursokotlin.appinstaclone.IgViewModel
 import com.cursokotlin.appinstaclone.main.composables.BottomNavigationItem
@@ -12,9 +23,73 @@ import com.cursokotlin.appinstaclone.main.composables.BottomNavigationMenu
 
 @Composable
 fun MyPostsScreen(navController: NavController, vm: IgViewModel) {
-    Column(modifier = Modifier.fillMaxSize()) {
+
+    val userData = vm.userData.value
+    val isLoading = vm.inProgress.value
+
+
+    Column {
         Column(modifier = Modifier.weight(1f)) {
-            Text(text = "MyPosts Screen")
+            Row {
+                ProfileImage(userData?.imageUrl) {
+
+                }
+
+                Text(
+                    text = "15\nposts", modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+
+                Text(
+                    text = "54\nfollowers", modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+
+                Text(
+                    text = "93\nfollowing", modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+
+            }
+            Column(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                val usernameDisplay =
+                    if (userData?.username == null) "" else "@${userData?.username}"
+                Text(text = userData?.name ?: "", fontWeight = FontWeight.Bold)
+                Text(text = usernameDisplay)
+                Text(text = userData?.bio ?: "")
+
+            }
+            OutlinedButton(
+                onClick = { },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent,
+                    contentColor = Color.LightGray),
+                elevation = ButtonDefaults.elevatedButtonElevation(
+                    defaultElevation = 0.dp,
+                    pressedElevation = 0.dp,
+                    disabledElevation = 0.dp
+                ),
+                shape = RoundedCornerShape(10),
+
+            ) {
+                Text(text = "Edit Profile", color = Color.Black)
+            }
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Posts lists")
+
+            }
         }
         BottomNavigationMenu(
             selectedItem = BottomNavigationItem.POSTS,
@@ -22,4 +97,9 @@ fun MyPostsScreen(navController: NavController, vm: IgViewModel) {
         )
 
     }
+}
+
+@Composable
+fun ProfileImage(imageUrl: String?, onClick: () -> Unit) {
+
 }
