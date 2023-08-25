114a423206c3979d3d07ceefb92ab119c84e360f
Mj-br <98741174+Mj-br@users.noreply.github.com>
2023-08-25 18:46:45 +0200

    Initiating the NewPostsScreen.kt

--- app/src/main/java/com/cursokotlin/appinstaclone/main/composables/screens/NewPostsScreen.kt
+++ app/src/main/java/com/cursokotlin/appinstaclone/main/composables/screens/NewPostsScreen.kt
@@ -1,11 +1,23 @@
 package com.cursokotlin.appinstaclone.main.composables.screens
 
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.navigation.NavController
 import com.cursokotlin.appinstaclone.IgViewModel
 
 @Composable
 fun NewPostsScreen(navController: NavController, viewModel: IgViewModel, encodeUri: String) {
+
+    val imageUri by remember{ mutableStateOf(encodeUri)}
+    var description by rememberSaveable { mutableStateOf("") }
+    val scrollState = rememberScrollState()
+    val focusManager = LocalFocusManager.current
 
 
 }
