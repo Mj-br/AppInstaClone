a60a8087c021e241e43c15dd41aabfc615d84602
Mj-br <98741174+Mj-br@users.noreply.github.com>
2023-09-02 02:53:51 +0200

    Starting the SinglePostScreen.kt

--- app/src/main/java/com/cursokotlin/appinstaclone/main/composables/screens/SinglePostScreen.kt
+++ app/src/main/java/com/cursokotlin/appinstaclone/main/composables/screens/SinglePostScreen.kt
@@ -1,8 +1,17 @@
 package com.cursokotlin.appinstaclone.main.composables.screens
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.cursokotlin.appinstaclone.IgViewModel
+import com.cursokotlin.appinstaclone.data.PostData
 
 @Composable
-fun SinglePostScreen(){
+fun SinglePostScreen(
+    navController: NavController,
+    vm: IgViewModel,
+    post: PostData
+) {
+    Text(text = "Single post screen ${post.postDescription}")
 
 }
