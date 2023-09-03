fad67d6b3809f6dc6038b312b637eab8d4746023
Mj-br <98741174+Mj-br@users.noreply.github.com>
2023-09-03 17:25:02 +0200

    Adding functionality to the SinglePostScreen.kt and discovered a bug that cant allow me to have the userId.

--- app/src/main/java/com/cursokotlin/appinstaclone/main/composables/screens/SinglePostScreen.kt
+++ app/src/main/java/com/cursokotlin/appinstaclone/main/composables/screens/SinglePostScreen.kt
@@ -1,16 +1,34 @@
 package com.cursokotlin.appinstaclone.main.composables.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.cursokotlin.appinstaclone.IgViewModel
 import com.cursokotlin.appinstaclone.data.PostData
+import com.cursokotlin.appinstaclone.main.composables.CommonDivider
 import com.cursokotlin.appinstaclone.main.composables.CommonImage
+import com.cursokotlin.appinstaclone.main.composables.UserImageCard
+import java.nio.file.WatchEvent
 
 @Composable
 fun SinglePostScreen(
@@ -18,8 +36,68 @@ fun SinglePostScreen(
     vm: IgViewModel,
     post: PostData?
 ) {
-    Text(text = "Single post screen ${post?.postDescription}")
-    
-    PostImage(imageUrl = post?.postImage, modifier = Modifier.fillMaxWidth().padding(64.dp) )
+    post?.userId?.let {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(8.dp)
+        ) {
+            Text(text = "Back", modifier = Modifier.clickable { navController.popBackStack() })
+
+            CommonDivider()
+
+            SinglePostDisplay(navController = navController, vm = vm, post = post)
+
+
+        }
+    }
+
+}
+
+@Composable
+fun SinglePostDisplay(navController: NavController, vm: IgViewModel, post: PostData) {
+    val userData = vm.userData.value
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            UserImageCard(
+                userImage = post.userImage,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(32.dp)
+            )
+            Text(text = post.username ?: "")
+            Text(text = ".", modifier = Modifier.padding(8.dp))
+
+            if (userData?.userId == post.userId) {
+                // Current user's post. Don't show anything
+            } else {
+                Text(text = "Follow", color = Color.Blue, modifier = Modifier.clickable {
+                    // Follow a user
+                })
+            }
+        }
+
+    }
+    Box {
+        val modifier = Modifier
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = 150.dp)
+        CommonImage(
+            data = post.postImage,
+            modifier = modifier,
+            contentScale = ContentScale.FillWidth
+        )
+    }
 
 }
