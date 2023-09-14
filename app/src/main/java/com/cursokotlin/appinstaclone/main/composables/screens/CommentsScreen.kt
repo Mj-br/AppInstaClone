1bcbe2f4e749db036417766cf18d3d8243c57d02
Manuel Rodriguez <ro7josemanuel@gmail.com>
2023-09-15 00:44:10 +0200

    Update

--- app/src/main/java/com/cursokotlin/appinstaclone/main/composables/screens/CommentsScreen.kt
+++ app/src/main/java/com/cursokotlin/appinstaclone/main/composables/screens/CommentsScreen.kt
@@ -41,6 +41,7 @@ fun CommentsScreen(navController: NavController, vm: IgViewModel, postId: String
     val comments = vm.comments.value
     val commentsProgress = vm.commentsProgress.value
 
+
     Column(modifier = Modifier.fillMaxSize()) {
 
         if (commentsProgress) {
